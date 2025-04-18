21ea4bebd2833b3da6e8e0f17559a2ad585727b1
Pedro Hilario <flygames1214@gmail.com>
2025-04-18 20:30:58 -0300

    Criando layout do card de ListPost

--- components/ListPosts/ListPosts.jsx
+++ components/ListPosts/ListPosts.jsx
@@ -1,8 +1,9 @@
 'use client'
 
-import './ListPosts.module.css';
+import styles from './ListPosts.module.css';
 import { useEffect, useState } from 'react';
 import { getPosts } from '@/services/posts';
+import { Avatar } from '@chakra-ui/react';
 
 const ListPost = () =>{
     const [posts, setPosts] = useState([]);
@@ -11,15 +12,21 @@ const ListPost = () =>{
         getPosts().then(data => setPosts(data))
     },[])
 
-    console.log(posts)
     return (
-        <ul>
-            {posts.map((post) => (
-                <li key={post.id}>{post.title}</li>
-            ))}
-        </ul>
+        <div className={styles.containerPost}>
+            <div className={styles.postHead}>
+                <Avatar.Root>
+                    <Avatar.Fallback name="Pedro Hilário" />
+                </Avatar.Root>
+                <h3 className={styles.postName}>Leanne Graham</h3>
+                <h4 className={styles.postUser}>@Bret</h4>
+            </div>
+            <div  className={styles.containerContent}>
+                <h3 className={styles.contentTitle}>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h3>
+                <p className={styles.contentBody}>quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto</p>
+            </div>
+        </div>
     );
-    
 }
 
 export default ListPost;
